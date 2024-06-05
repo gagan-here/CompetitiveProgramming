2925950b955e106fda8ef3468c21bfdd0394c182
gagan-here <chatwidmeah@gmail.com>
2024-06-05 12:15:10 +0545

    initial commit for number of islands problem

---
+++ Blind75/Graph/NumberOfIslands.java
@@ -0,0 +1,30 @@
+package Blind75.Graph;
+
+/**
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+ * <p>
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: grid = [ <br>
+ * ["1","1","1","1","0"], <br>
+ * ["1","1","0","1","0"], <br>
+ * ["1","1","0","0","0"], <br>
+ * ["0","0","0","0","0"] <br>
+ * ] <p>
+ *
+ * Output: 1 <p>
+ * Example 2: <br>
+ * Input: grid = [ <br>
+ * ["1","1","0","0","0"], <br>
+ * ["1","1","0","0","0"], <br>
+ * ["0","0","1","0","0"], <br>
+ * ["0","0","0","1","1"] <br>
+ * ] <br>
+ * Output: 3
+ */
+public class NumberOfIslands {
+}
