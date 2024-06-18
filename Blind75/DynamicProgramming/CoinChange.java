0180e2edc67e4b2ffe34fcaebc7e307a9c653aaa
gagan-here <chatwidmeah@gmail.com>
2024-06-18 21:17:31 +0545

    added solution code for coin change problem

--- Blind75/DynamicProgramming/CoinChange.java
+++ Blind75/DynamicProgramming/CoinChange.java
@@ -1,5 +1,7 @@
 package Blind75.DynamicProgramming;
 
+import java.util.Arrays;
+
 /**
  * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
  * <p>
@@ -26,5 +28,25 @@ package Blind75.DynamicProgramming;
  * Output: 0
  */
 public class CoinChange {
+    public static int numOfCoins(int[] coins, int amount) {
+        int[] minCoins = new int[amount + 1];
+        Arrays.fill(minCoins, amount + 1);
+        minCoins[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    minCoins[i] = Math.min(minCoins[i], 1 + minCoins[i - coin]);
+                }
+            }
+        }
+        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        int total = numOfCoins(coins, amount);
+        System.out.println("Number of coins needed to make up amount "+ amount + " is: "+total);
+    }
 }
 
