ed00e766bc7c01801727a18abb7878a073845b07
gagan-here <chatwidmeah@gmail.com>
2024-06-18 16:15:11 +0545

    initial commit for coin change

---
+++ Blind75/DynamicProgramming/CoinChange.java
@@ -0,0 +1,30 @@
+package Blind75.DynamicProgramming;
+
+/**
+ * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+ * <p>
+ * Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ * <p>
+ * You may assume that you have an infinite number of each kind of coin.
+ * <p>
+ * Example 1:
+ * <br>
+ * Input: coins = [1,2,5], amount = 11 <br>
+ * Output: 3 <br>
+ * Explanation: 11 = 5 + 5 + 1
+ * <p>
+ * <p>
+ * Example 2:
+ * <br>
+ * Input: coins = [2], amount = 3 <br>
+ * Output: -1 <br>
+ *
+ * <p>
+ * Example 3:
+ * <br>
+ * Input: coins = [1], amount = 0 <br>
+ * Output: 0
+ */
+public class CoinChange {
+}
+
