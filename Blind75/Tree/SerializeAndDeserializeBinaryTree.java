39c2857f4a499d75ceefa1bdde9becff9529c3e5
gagan-here <chatwidmeah@gmail.com>
2024-06-19 15:57:38 +0545

    added more information in SerializeAndDeserializeBinaryTree class

--- Blind75/Tree/SerializeAndDeserializeBinaryTree.java
+++ Blind75/Tree/SerializeAndDeserializeBinaryTree.java
@@ -9,11 +9,17 @@ package Blind75.Tree;
  * algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized
  * to the original tree structure.
  * <p>
- *           1 <br>
- *         /  \ <br>
- *        2    3 <br>
- *            / \ <br>
- *          4    5
+ * 1 <br>
+ * /  \ <br>
+ * 2    3 <br>
+ * / \ <br>
+ * 4    5
+ * <p>
+ * In serialization, you will be given a binary tree like above and you have to convert it to a string.<br>
+ * For above binary tree the string while serializing would be [1,2,3,#,#,4,5,#,#,#,#] <br>
+ * Here, # means the root node doesn't have a left or right sub-tree
+ * <p>
+ * Similarly, while deserializing the string [1,2,3,#,#,4,5,#,#,#,#] should be converted to binary tree like above
  */
 public class SerializeAndDeserializeBinaryTree {
 
