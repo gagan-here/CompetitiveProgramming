d7f7f8d86089e9ff66932d18f62b6982aa9d0957
gagan-here <chatwidmeah@gmail.com>
2024-06-23 15:40:01 +0545

    initial commit for MergeIntervals

---
+++ Blind75/Intervals/MergeIntervals.java
@@ -0,0 +1,19 @@
+package Blind75.Intervals;
+
+/**
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: intervals = [[1,3],[2,6],[8,10],[15,18]] <br>
+ * Output: [[1,6],[8,10],[15,18]] <br>
+ * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: intervals = [[1,4],[4,5]] <br>
+ * Output: [[1,5]] <br>
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ */
+public class MergeIntervals {
+}
