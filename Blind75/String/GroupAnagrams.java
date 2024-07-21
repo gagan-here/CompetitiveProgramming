d7c0037165d8b93d00cc17b19903eae171711cd9
Gagan Basnet <chatwidmeah@gmail.com>
2024-07-21 23:54:56 +0545

    added initial commit for group anagram

---
+++ Blind75/String/GroupAnagrams.java
@@ -0,0 +1,24 @@
+package Blind75.String;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * <p>
+ * <b>Example 1:</b>
+ * <p>
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"] <br>
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]] <br>
+ * <p>
+ * <b>Example 2:</b>
+ * <p>
+ * Input: strs = [""] <br>
+ * Output: [[""]] <br>
+ * <p>
+ * <b>Example 3:</b>
+ * <p>
+ * Input: strs = ["a"] <br>
+ * Output: [["a"]]
+ */
+public class GroupAnagrams {
+}
