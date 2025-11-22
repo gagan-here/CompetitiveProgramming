982035e48d639192092ac14eb42e2bc9f17cde51
Gagan Basnet <chatwidmeah@gmail.com>
2025-11-22 23:09:43 +0545

    add dfsOfGraph method to return DFS traversal from node 0

--- Blind75/Graph/DFS.java
+++ Blind75/Graph/DFS.java
@@ -1,4 +1,24 @@
 package Blind75.Graph;
 
+import java.util.ArrayList;
+
 public class DFS {
+    public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ls) {
+    }
+
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ls = new ArrayList<>();
+
+        if (V == 0) {
+            return ls;
+        }
+
+        boolean vis[] = new boolean[V + 1];
+        vis[0] = true;
+        dfs(0, vis, adj, ls);
+
+        return ls;
+    }
+
 }
+
