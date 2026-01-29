bc2d4d0887167c963e8919563a8a05f7be8a0e5a
Gagan Basnet <chatwidmeah@gmail.com>
2026-01-29 22:24:25 +0545

    Added input validation to handle edge cases for empty or null matrices.

--- Blind75/Graph/PacificAtlanticWaterFlow.java
+++ Blind75/Graph/PacificAtlanticWaterFlow.java
@@ -1,4 +1,14 @@
 package Blind75.Graph;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PacificAtlanticWaterFlow {
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (heights == null || heights.length == 0 || heights[0].length == 0)
+            return res;
+    }
 }
